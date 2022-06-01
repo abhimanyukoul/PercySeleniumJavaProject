7d530e4eef12f0b9ef268e74fcfb8c667c57ce88
abhimanyukoul <abhimanyu@browserstack.com>
2022-06-01 19:05:04 +0530

    Initial Commit - Percy Selenium Java Execution on BrowserStack Platform

---
+++ src/test/java/org/percyScripts/myFirstPercySeleniumScriptTest.java
@@ -0,0 +1,118 @@
+package org.percyScripts;
+
+import io.percy.selenium.Percy;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.remote.LocalFileDetector;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.URL;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class myFirstPercySeleniumScriptTest {
+
+    public static RemoteWebDriver remoteWebDriver = null;
+    public static ChromeOptions chromeOptions = null;
+    public static JavascriptExecutor jsExecutor = null;
+    public static WebDriverWait explicitWait = null;
+    public static Percy percy = null;
+    public static final String USERNAME = System.getenv("BROWSERSTACK_USERNAME");
+    public static final String PASSWORD = System.getenv("BROWSERSTACK_ACCESS_KEY");
+    public static final String URL = "https://" + USERNAME + ":" + PASSWORD + "@hub-cloud.browserstack.com/wd/hub";
+
+    @Before
+    public void setExecutionEnvironmentBeforeTest() throws Exception{
+        try {
+            chromeOptions = new ChromeOptions();
+            chromeOptions.setCapability("os", "OS X");
+            chromeOptions.setCapability("os_version", "Monterey");
+            chromeOptions.setCapability("browser", "Chrome");
+            chromeOptions.setCapability("browser_version", "latest");
+            chromeOptions.setCapability("project", "assignment");
+            chromeOptions.setCapability("build", "Percy Tests");
+            chromeOptions.setCapability("name", "executeFileUploadScenarioPercyTest");
+            chromeOptions.setCapability("browserstack.local", "false");
+            chromeOptions.setCapability("browserstack.debug", "true");
+            chromeOptions.setCapability("browserstack.console", "info");
+            chromeOptions.setCapability("browserstack.networkLogs", "true");
+            chromeOptions.setCapability("browserstack.selenium_version", "4.1.0");
+            chromeOptions.setCapability("browserstack.telemetryLogs", "true");
+
+            remoteWebDriver = new RemoteWebDriver(new URL(URL), chromeOptions);
+            jsExecutor = (JavascriptExecutor)remoteWebDriver;
+            remoteWebDriver.setFileDetector(new LocalFileDetector());
+            percy = new Percy(remoteWebDriver);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void executeFileUploadScenarioPercyTest () throws Exception{
+        try {
+            remoteWebDriver.get("https://the-internet.herokuapp.com/upload");
+            remoteWebDriver.manage().window().maximize();
+            remoteWebDriver.manage().deleteAllCookies();
+            remoteWebDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+            List<Integer> snapshotsWidth = new ArrayList<Integer>();
+            snapshotsWidth.add(375);
+            snapshotsWidth.add(1280);
+
+            percy.snapshot("Herokuapp Upload Page", snapshotsWidth, 1024);
+
+            explicitWait = new WebDriverWait(remoteWebDriver, Duration.ofSeconds(30));
+            explicitWait.until((ExpectedCondition<Boolean>) remoteWebDriver -> (jsExecutor.executeScript("return document.readyState").toString().equalsIgnoreCase("complete")));
+
+            jsExecutor.executeScript("console.log('Writing logs on Browser Console through Selenium script')");
+            jsExecutor.executeScript("console.warn('Warning!')");
+            jsExecutor.executeScript("console.error('Error!')");
+
+            LogEntries logEntries = remoteWebDriver.manage().logs().get(LogType.BROWSER);
+            for (LogEntry entry : logEntries) {
+                System.out.println(new Date(entry.getTimestamp()) + " " + entry.getLevel() + " " + entry.getMessage());
+            }
+
+            remoteWebDriver.findElement(By.cssSelector("input#file-upload")).sendKeys("<path of the file to be uploaded including file.extension>");
+            remoteWebDriver.findElement(By.cssSelector("input#file-submit")).click();
+            explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='example']/h3[text()='File Uploaded!']")));
+
+            percy.snapshot("Herokuapp File Upload Success Page", snapshotsWidth, 1024);
+
+            if (remoteWebDriver.findElement(By.xpath("//div[@class='example']/h3[text()='File Uploaded!']")).getText().equalsIgnoreCase("File Uploaded!")) {
+                jsExecutor.executeScript("browserstack_executor:    {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"File uploaded successfully\"}}");
+                Assert.assertTrue(true);
+            }
+            else {
+                jsExecutor.executeScript("browserstack_executor:    {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"failed\", \"reason\": \"File upload failed \"}}");
+                Assert.fail();
+            }
+        }
+        catch (Exception e) {
+            jsExecutor.executeScript("browserstack_executor:    {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"failed\", \"reason\": \"File could not be uploaded in this session\"}}");
+            e.printStackTrace();
+        }
+    }
+
+    @After
+    public void clearExecutionEnvironmentAfterTest() throws Exception{
+        jsExecutor = null;
+        remoteWebDriver.quit();
+    }
+
+}
---
+++ src/test/java/org/percyScripts/mySecondPercySeleniumScriptLocalTest.java
@@ -0,0 +1,138 @@
+package org.percyScripts;
+
+import com.browserstack.local.Local;
+import io.percy.selenium.Percy;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.URL;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class mySecondPercySeleniumScriptLocalTest {
+
+    public static RemoteWebDriver remoteWebDriver = null;
+    public static ChromeOptions chromeOptions = null;
+    public static JavascriptExecutor jsExecutor = null;
+    public static WebDriverWait explicitWait = null;
+    public static Percy percy = null;
+    public static Local bsLocal = null;
+    public static final String USERNAME = System.getenv("BROWSERSTACK_USERNAME");
+    public static final String PASSWORD = System.getenv("BROWSERSTACK_ACCESS_KEY");
+    public static final String URL = "https://" + USERNAME + ":" + PASSWORD + "@hub-cloud.browserstack.com/wd/hub";
+
+    @Before
+    public void setExecutionEnvironmentBeforeTest() throws Exception{
+        try {
+            // Creates an instance of Local
+            bsLocal = new Local();
+
+            HashMap<String, String> bsLocalArgs = new HashMap<String, String>();
+            bsLocalArgs.put("key", PASSWORD);
+            bsLocalArgs.put("localIdentifier", "percySeleniumLocalTest");
+            bsLocalArgs.put("forcelocal", "true");
+
+            bsLocal.start(bsLocalArgs);
+
+            chromeOptions = new ChromeOptions();
+            chromeOptions.setCapability("os", "OS X");
+            chromeOptions.setCapability("os_version", "Monterey");
+            chromeOptions.setCapability("browser", "Chrome");
+            chromeOptions.setCapability("browser_version", "latest");
+            chromeOptions.setCapability("project", "assignment");
+            chromeOptions.setCapability("build", "Percy Tests");
+            chromeOptions.setCapability("name", "dynamicTimerDataSiteScenarioPercyTest");
+            chromeOptions.setCapability("browserstack.local", "true");
+            chromeOptions.setCapability("browserstack.localIdentifier", "percySeleniumLocalTest");
+            chromeOptions.setCapability("browserstack.debug", "true");
+            chromeOptions.setCapability("browserstack.console", "info");
+            chromeOptions.setCapability("browserstack.networkLogs", "true");
+            chromeOptions.setCapability("browserstack.selenium_version", "4.1.0");
+            chromeOptions.setCapability("browserstack.telemetryLogs", "true");
+
+            remoteWebDriver = new RemoteWebDriver(new URL(URL), chromeOptions);
+            jsExecutor = (JavascriptExecutor)remoteWebDriver;
+            percy = new Percy(remoteWebDriver);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void dynamicTimerDataSiteScenarioPercyTest () throws Exception{
+        try {
+            // Check if BrowserStack local instance is running
+            System.out.println(bsLocal.isRunning());
+
+            remoteWebDriver.get("http://<username>:<password>@localhost/temp.html");
+            remoteWebDriver.manage().window().maximize();
+            remoteWebDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+            Thread.sleep(10000);
+
+            List<Integer> snapshotsWidth = new ArrayList<Integer>();
+            snapshotsWidth.add(375);
+            snapshotsWidth.add(768);
+            snapshotsWidth.add(1280);
+
+            percy.snapshot("HTML Assignment Webpage - Local - One", snapshotsWidth, 1024, false, "p#time {visibility: hidden;}");
+
+            explicitWait = new WebDriverWait(remoteWebDriver, Duration.ofSeconds(30));
+            explicitWait.until((ExpectedCondition<Boolean>) remoteWebDriver -> (jsExecutor.executeScript("return document.readyState").toString().equalsIgnoreCase("complete")));
+
+            remoteWebDriver.findElement(By.cssSelector("input#username")).sendKeys("<username>");
+            remoteWebDriver.findElement(By.cssSelector("input#password")).sendKeys("<password>");
+            remoteWebDriver.findElement(By.cssSelector("input#signIn")).click();
+            explicitWait.until((ExpectedCondition<Boolean>) remoteWebDriver -> (jsExecutor.executeScript("return document.readyState").toString().equalsIgnoreCase("complete")));
+
+            remoteWebDriver.findElement(By.cssSelector("input#Male")).click();
+            Thread.sleep(5000);
+            remoteWebDriver.findElement(By.cssSelector("input#Pizza")).click();
+            remoteWebDriver.findElement(By.cssSelector("input#Burger")).click();
+            Thread.sleep(5000);
+
+            Select dropdown = new Select(remoteWebDriver.findElement(By.cssSelector("select#selectSports")));
+            dropdown.selectByVisibleText("Basketball");
+            Thread.sleep(5000);
+            dropdown.selectByIndex(3);
+            Thread.sleep(5000);
+
+            jsExecutor.executeScript("window.scrollBy(0, 750)");
+            Thread.sleep(5000);
+            jsExecutor.executeScript("window.scrollBy(0, -750)");
+            Thread.sleep(5000);
+
+            percy.snapshot("HTML Assignment Webpage - Local - Two", snapshotsWidth, 1024, false, "p#time {visibility: hidden;}");
+            Thread.sleep(5000);
+
+            jsExecutor.executeScript("browserstack_executor:    {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"All Local Testing steps completed successfully\"}}");
+
+            // Check if BrowserStack local instance is running
+            System.out.println(bsLocal.isRunning());
+        }
+        catch (Exception e) {
+            jsExecutor.executeScript("browserstack_executor:    {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"failed\", \"reason\": \"All Local Testing steps not completed successfully\"}}");
+            e.printStackTrace();
+        }
+    }
+
+    @After
+    public void clearExecutionEnvironmentAfterTest() throws Exception{
+        jsExecutor = null;
+        remoteWebDriver.quit();
+
+        bsLocal.stop();
+    }
+
+}
